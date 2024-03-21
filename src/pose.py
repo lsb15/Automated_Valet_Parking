f9c5c7af018e8a15eba54b697a9ea34a11efcd4f
Sangbeen Lee <163862686+lsb15@users.noreply.github.com>
2024-03-21 16:53:26 +0900

    Update pose.py

--- src/pose.py
+++ src/pose.py
@@ -1,10 +1,13 @@
-"""
-Hybrid A Star Pose
-"""
-
-
 class Pose:
     def __init__(self, x, y, theta):
-        self.x = x
-        self.y = y
-        self.theta = theta
+        """
+        Constructor for creating a Pose object.
+
+        Args:
+        - x (float): The x-coordinate of the pose.
+        - y (float): The y-coordinate of the pose.
+        - theta (float): The orientation (in radians) of the pose.
+        """
+        self.x = x # x-coordinate of the pose
+        self.y = y # y-coordinate of the pose
+        self.theta = theta # orientation (in radians) of the pose
